62e78971622218ba9404e966f34af0e1e025f429
tritschbergera <antoine.tritschberger@etu.unistra.fr>
2021-10-25 16:01:32 +0200

    Ajout des fichiers de base

---
+++ script.js
@@ -0,0 +1,7 @@
+$("#envoyer").on('click',function(e){
+    let p1 = $("#password")[0].value;
+    let p2 = $("#password2")[0].value;
+    if(p1!==p2){
+        $("#error").html("Les 2 mots de passe sont différents");
+    }
+});
