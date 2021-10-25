74807eb037566b6a11d2362039c6a648cba76eb1
tritschbergera <antoine.tritschberger@etu.unistra.fr>
2021-10-25 16:36:35 +0200

    Presque Less

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-$("#envoyer").on('click',function(e){
+onSubmit=()=>{
     let p1 = $("#password")[0].value;
     let p2 = $("#password2")[0].value;
     if(p1!==p2){
         $("#error").html("Les 2 mots de passe sont différents");
+        return false;
     }
-});
+    return true;
+};
